7f96b4480edb2c1ae9af003f01769be2ed3d99fc
Daniel Koch <koch@terrestris.de>
2019-04-02 10:34:55 +0200

    Fix some typos

--- src/HttpUtil/HttpUtil.ts
+++ src/HttpUtil/HttpUtil.ts
@@ -25,14 +25,14 @@ class HttpUtil {
    * optimized for the usage with a shogun-core backend).
    *
    * @param {Object} opts The options object to configure the post request.
-   *  It can contain theses keys:
+   *  It can contain the following keys:
    *    {String} url The url we want to send the post to.
    *    {Object} params The post params we want to send. Default is to {}.
    *    {Object} additionalHeaders An object with additional headers as kvp.
    *      Default is to {}.
    *    {Object} additionalFetchOptions An object containing additional options
    *      for the fetch API. Compare https://mdn.io/fetch. Default is to {}.
-   *    {Boolean} sameOriginCredentials A flag to wether set the credentials
+   *    {Boolean} sameOriginCredentials A flag to whether set the credentials
    *      option to 'same-origin' or let it undefined. Default is to true.
    *    {Boolean} asForm A flag to set the Content-Type header to
    *      'application/x-www-form-urlencoded'. Default is to true.
